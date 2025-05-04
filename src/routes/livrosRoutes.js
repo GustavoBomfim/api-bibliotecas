9b8f64c7ad8c2c64a012249c9dd943ee194ee12d
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2025-05-04 20:16:53 -0300

    Adicionando endpoint para buscar livros filtrados por editora

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,11 +4,11 @@ import LivroController from "../controllers/livroController.js";
 const routes = express.Router();
 
 routes.get("/livros", LivroController.listarLivros);
+routes.get("/livros/busca", LivroController.listarLivrosPorEditora);
 routes.get("/livros/:id", LivroController.listarLivroPorId);
 routes.post("/livros", LivroController.cadastrarLivro);
 routes.put("/livros/:id", LivroController.atualizarLivroPorId);
 routes.delete("/livros/:id", LivroController.excluirLivroPorId);
-
 
 
 export default routes;
